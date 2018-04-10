dd6092bfae5f1a39501247a8653ddd3933b33d30
Régis Behmo <regis@behmo.com>
2018-04-10 07:50:41 +0200

    Set proper extension on file attachment download
    On attachment download, file extensions were no longer correctly set.
    This was inconvenient because file extension could not be properly set.
    For instance, instead of downloading "myattachment.pdf", the filename
    was set to "hvriuehvrhjzkjpeofu1345RFV76cgfc" (without the ".pdf"
    suffix).
    
    This regression was introduced when file keys stopped referring to file
    names. Now, we can no longer retrieve the original file name, just the
    extension.
    
    Note that the proposed fix works only for the "filesystem" backend: for
    the "django" backend, we do not store the metadata associated to the
    uploaded files. Thus, the original file content type is lost after the
    upload.
    
    This issue was raised a couple times publicly, although no formal issue
    was created in the github repo:
    2017-03: https://groups.google.com/forum/#!topic/edx-code/YJuNMeIaJi0
    2018-03: https://openedx.slack.com/messages/C08B4LZEZ/convo/C08B4LZEZ-1521550202.000508/?thread_ts=1521550202.000508
    2018-09: https://openedx.slack.com/messages/C08B4LZEZ/convo/C08B4LZEZ-1537270442.000100/

--- openassessment/fileupload/backends/base.py
+++ openassessment/fileupload/backends/base.py
@@ -1,4 +1,5 @@
 import abc
+import mimetypes
 
 from django.conf import settings
 
@@ -18,7 +19,16 @@ class Settings(object):
         stored file names. The specified file prefix for the storage must be
         publicly viewable or all uploaded files will not be seen.
     """
-    DEFAULT_FILE_UPLOAD_STORAGE_PREFIX = "submissions_attachments"
+    DEFAULT_FILE_UPLOAD_STORAGE_PREFIX = "submissions_attachments"  # pylint: disable=invalid-name
+    FILE_EXTENSIONS_BY_TYPE = {
+        'image/gif': '.gif',
+        'image/jpeg': '.jpg',
+        'image/pjpeg': '.jpg',
+        'image/png': '.png',
+        'application/pdf': '.pdf',
+        'application/msword': '.doc',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document': '.docx'
+    }
 
     @classmethod
     def get_bucket_name(cls):
@@ -34,6 +44,16 @@ class Settings(object):
         Defaults to the DEFAULT_FILE_UPLOAD_STORAGE_PREFIX class attribute.
         """
         return getattr(settings, "FILE_UPLOAD_STORAGE_PREFIX", cls.DEFAULT_FILE_UPLOAD_STORAGE_PREFIX)
+
+    @classmethod
+    def guess_extension(cls, mime_type):
+        """
+        Guess a file extension (with a leading dot) given its mime type. If no
+        file is found, return an empty extension.
+        """
+        if mime_type in cls.FILE_EXTENSIONS_BY_TYPE:
+            return cls.FILE_EXTENSIONS_BY_TYPE[mime_type]
+        return mimetypes.guess_extension(mime_type) or ''
 
 
 class BaseBackend(object):
